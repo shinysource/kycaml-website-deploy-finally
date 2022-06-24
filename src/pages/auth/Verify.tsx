23cfb2dee24e31ab158f689d71497579986f5135
shinysource <highly.flores@gmail.com>
2022-06-24 08:54:00 -0700

    Fixed some issues

--- src/pages/auth/Verify.tsx
+++ src/pages/auth/Verify.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, FormEvent } from "react";
+import { useState, useEffect, FormEvent, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
@@ -25,31 +25,34 @@ const Signup = () => {
   const [accessSDKToken, setAccessSDKToken] = useState<string>("");
   const [applicantEmail, setApplicantEmail] = useState<string>("");
 
-  const config = {
-    lang: "en",
-    email: applicantEmail,
-    i18n: {
-      document: {
-        subTitles: {
-          IDENTITY: "Upload a document that proves your identity",
+  const config = useMemo(
+    () => ({
+      lang: "en",
+      email: applicantEmail,
+      i18n: {
+        document: {
+          subTitles: {
+            IDENTITY: "Upload a document that proves your identity",
+          },
         },
       },
-    },
-    onMessage: (type: String, payload: any) => {
-      console.log("WebSDK onMessage", type, payload);
-    },
-    uiConf: {
-      customCssStr:
-        ":root {\n  --black: #000000;\n   --white: #FFFFFF;\n  --grey-darker: #B2B2B2;\n  --border-color: #DBDBDB;\n}\n\np {\n  color: var(--black);\n  font-size: 16px;\n  line-height: 24px;\n}\n\nsection {\n  margin: 40px auto;\n}\n\ninput {\n  color: var(--black);\n  font-weight: 600;\n  outline: none;\n}\n\nsection.content {\n  background-color: var(--white);\n  color: var(--black);\n  padding: 40px 40px 16px;\n  box-shadow: none;\n  border-radius: 6px;\n}\n\nbutton.submit,\nbutton.back {\n  text-transform: capitalize;\n  border-radius: 6px;\n  height: 48px;\n  padding: 0 30px;\n  font-size: 16px;\n  background-image: none !important;\n  transform: none !important;\n  box-shadow: none !important;\n  transition: all 0.2s linear;\n}\n\nbutton.submit {\n  min-width: 132px;\n  background: none;\n  background-color: var(--black);\n}\n\n.round-icon {\n  background-color: var(--black) !important;\n  background-image: none !important;\n}",
-    },
-    onError: (error: String) => {
-      console.error("WebSDK onError", error);
-    },
-  };
+      onMessage: (type: String, payload: any) => {
+        console.log("WebSDK onMessage", type, payload);
+      },
+      uiConf: {
+        customCssStr:
+          ":root {\n  --black: #000000;\n   --white: #FFFFFF;\n  --grey-darker: #B2B2B2;\n  --border-color: #DBDBDB;\n}\n\np {\n  color: var(--black);\n  font-size: 16px;\n  line-height: 24px;\n}\n\nsection {\n  margin: 40px auto;\n}\n\ninput {\n  color: var(--black);\n  font-weight: 600;\n  outline: none;\n}\n\nsection.content {\n  background-color: var(--white);\n  color: var(--black);\n  padding: 40px 40px 16px;\n  box-shadow: none;\n  border-radius: 6px;\n}\n\nbutton.submit,\nbutton.back {\n  text-transform: capitalize;\n  border-radius: 6px;\n  height: 48px;\n  padding: 0 30px;\n  font-size: 16px;\n  background-image: none !important;\n  transform: none !important;\n  box-shadow: none !important;\n  transition: all 0.2s linear;\n}\n\nbutton.submit {\n  min-width: 132px;\n  background: none;\n  background-color: var(--black);\n}\n\n.round-icon {\n  background-color: var(--black) !important;\n  background-image: none !important;\n}",
+      },
+      onError: (error: String) => {
+        console.error("WebSDK onError", error);
+      },
+    }),
+    [applicantEmail]
+  );
 
   useEffect(() => {
     config.email = applicantEmail;
-  }, [applicantEmail]);
+  }, [applicantEmail, config]);
 
   const handler = () => Promise.resolve<string>("");
 
