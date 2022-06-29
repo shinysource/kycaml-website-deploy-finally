334e2f55954955f1e04976942b84e27ee19eb801
shinysource <highly.flores@gmail.com>
2022-06-29 06:48:56 -0700

    Updated home page ui

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -4,14 +4,18 @@ import { Link } from "react-router-dom";
 
 import CustomButton from "../../components/Button/CustomButton";
 import Footer from "../../layout/Footer";
-import { ArrowAnim } from "../../components/Animations/Animations";
 
 const Home = () => {
   return (
     <>
-      <div className="text-white">
-        <Grid container alignItems="center" className="py-10 home-back">
-          <Grid item container justifyContent="center" className="px-5">
+      <div className="text-white relative home-back">
+        <Grid container alignItems="start" className="">
+          <Grid
+            item
+            container
+            justifyContent="center"
+            className="px-5 py-10 bg-yellow"
+          >
             <Grid item>
               <Link to="/">
                 <img
@@ -27,32 +31,16 @@ const Home = () => {
             direction="column"
             justifyContent="center"
             alignItems="center"
+            className="text-center pb-[40px] bg-yellow"
           >
-            <div className="text-animate-content text-4xl font-normal text-center py-2 md:py-4 lg:py-4 w-full sm:w-full md:w-full lg:w-full">
-              <div className="text-animate-content-container">
-                <ul className="text-animate-content-container-list">
-                  <li className="hero-text text-animate-content-container-list-item uppercase mx-auto w-[180px] sm:w-[160px] lg:w-full">
-                    As part of your investment process
-                  </li>
-                  <li className="hero-text text-animate-content-container-list-item uppercase mx-auto w-[270px] sm:w-[240px] lg:w-full">
-                    Need to proceed with
-                  </li>
-                  <li className="hero-text text-animate-content-container-list-item uppercase mx-auto w-[270px] sm:w-[240px] lg:w-full">
-                    KYC / AML check
-                  </li>
-                  <li className="hero-text text-animate-content-container-list-item uppercase mx-auto w-[270px] sm:w-[240px] lg:w-full">
-                    Please click the button below
-                  </li>
-                  <li className="hero-text text-animate-content-container-list-item uppercase mx-auto w-[350px] sm:w-[360px] lg:w-full">
-                    Proceed through the brief KYC/AML process
-                  </li>
-                </ul>
-              </div>
+            <div className="hero-text text-center pt-2 mx-2">
+              Your Internal Ally For Fund Administration
             </div>
-            <div className="summary-text text-center pt-2 w-3/4">
-              Finally Is Your Internal Ally For Fund Administration
-            </div>
-            <div className="py-2 md:py-4 lg:py-4">
+            <p className="summary-text text-center w-4/5 pt-2">
+              Please click the button below to proceed through the brief KYC/AML
+              process
+            </p>
+            <div className="py-2 pt-8 lg:pt-[40px]">
               <Link to="/verify">
                 <CustomButton
                   style={{ width: "18rem", fontSize: "1.15rem" }}
@@ -66,48 +54,22 @@ const Home = () => {
               </Link>
             </div>
           </Grid>
-
-          <Grid container alignItems="flex-end">
-            <ArrowAnim />
-          </Grid>
-        </Grid>
-
-        <Grid
-          container
-          justifyContent="center"
-          className="text-center bg-white text-black"
-        >
-          <Grid item xs={12} pt={4}>
-            <p className="h2-text">Finally Fund Admin</p>
-          </Grid>
-          <Grid item xs={11} sm={8} lg={6} xl={4} pt={2}>
-            <p className="paragraph-text">
-              As part of your investment process, you will need to proceed with
-              a Know Your Customer/Anti Money Laundering check. Please click the
-              button below to proceed through the brief KYC/AML process
-            </p>
-          </Grid>
-          <Grid item container justifyContent="center" xs={12} pt={2}>
-            <Grid item className="pb-12">
-              <Link to="/verify">
-                <CustomButton
-                  style={{ width: "20rem", fontSize: "1.15rem" }}
-                  type="button"
-                  model="primary"
-                  variant="contained"
-                  label="Verify your identity"
-                  className="w-36 uppercase"
-                  endIcon={<ArrowForwardIosIcon sx={{ color: "#D3D3D3" }} />}
-                />
-              </Link>
+          <Grid item xs={12} className="pt-[30px] text-black">
+            <Grid item xs={12}>
+              <p className="font-podium49 text-center text-[32px] font-bold">
+                Let's Get You Verified!
+              </p>
+            </Grid>
+            <Grid item xs={12}>
+              <p className="summary-text text-center pb-[100px] w-2/3 mx-auto">
+                As part of your investment process, you will need to proceed
+                with a Know Your Customer/Anti Money Laundering check.
+              </p>
             </Grid>
           </Grid>
         </Grid>
-
-        <Grid container className="bg-black1">
-          <Grid item sx={{ width: "100%" }}>
-            <Footer />
-          </Grid>
+        <Grid container sx={{ width: "100%" }} className="absolute bottom-0">
+          <Footer />
         </Grid>
       </div>
     </>
